3f7161fe3333f10e4bd2acb5d68e9dc976d41785
Dalton Hubble <dghubble@gmail.com>
2016-03-17 14:33:56 -0700

    cmd/bootcfg: Start HTTP and gRPC (optional) via bootcfg binary

---
+++ bootcfg/rpc/grpc.go
@@ -0,0 +1,16 @@
+package rpc
+
+import (
+	"google.golang.org/grpc"
+
+	"github.com/coreos/coreos-baremetal/bootcfg/server"
+	pb "github.com/coreos/coreos-baremetal/bootcfg/server/serverpb"
+)
+
+// NewServer wraps the bootcfg Server to return a new gRPC Server.
+func NewServer(s server.Server, opts ...grpc.ServerOption) (*grpc.Server, error) {
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterGroupsServer(grpcServer, s)
+	pb.RegisterProfilesServer(grpcServer, s)
+	return grpcServer, nil
+}
