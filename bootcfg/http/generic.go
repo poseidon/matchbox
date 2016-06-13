95db4cba6108625d703a7bb7fa0b359cc3bbc88a
Adam Schaub <adam.m.schaub@gmail.com>
2016-06-13 15:01:31 -0400

    bootcfg/http: Added genericHandler + tests to the http server
    genericHandler uses a Profile.GenericId to locate and render a "generic" go template using group metadata and selectors.
    
    Partially addresses #224

---
+++ bootcfg/http/generic.go
@@ -0,0 +1,62 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"strings"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/coreos/coreos-baremetal/bootcfg/server"
+	pb "github.com/coreos/coreos-baremetal/bootcfg/server/serverpb"
+)
+
+// genericHandler returns a handler that responds with generic file for
+// the requester.
+func genericHandler(srv server.Server) ContextHandler {
+	fn := func(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+		group, err := groupFromContext(ctx)
+		if err != nil || group.Profile == "" {
+			http.NotFound(w, req)
+			return
+		}
+		profile, err := srv.ProfileGet(ctx, &pb.ProfileGetRequest{Id: group.Profile})
+		if err != nil || profile.GenericId == "" {
+			http.NotFound(w, req)
+			return
+		}
+		contents, err := srv.GenericGet(ctx, profile.GenericId)
+		if err != nil {
+			http.NotFound(w, req)
+			return
+		}
+
+		// collect data for rendering
+		data := make(map[string]interface{})
+		if group.Metadata != nil {
+			err = json.Unmarshal(group.Metadata, &data)
+			if err != nil {
+				log.Errorf("error unmarshalling metadata: %v", err)
+				http.NotFound(w, req)
+				return
+			}
+		}
+		for key, value := range group.Selector {
+			data[strings.ToLower(key)] = value
+		}
+
+		// render the template of a generic config with data
+		var buf bytes.Buffer
+		err = renderTemplate(&buf, data, contents)
+		if err != nil {
+			http.NotFound(w, req)
+			return
+		}
+
+		config := buf.String()
+		http.ServeContent(w, req, "", time.Time{}, strings.NewReader(config))
+	}
+	return ContextHandlerFunc(fn)
+}
