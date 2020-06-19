cc8515e1e78d4628d8d54dbbc5ac384e8829ad55
Spencer Owen <owenspencer@gmail.com>
2020-06-18 22:54:36 -0600

    add comments

--- examples/terraform/ubuntu-install/profiles.tf
+++ examples/terraform/ubuntu-install/profiles.tf
@@ -1,15 +1,33 @@
-resource "matchbox_profile" "ubuntu-18.04-install" {
+# Uses the ubuntu 'netboot' install option where ubuntu downloads needed packages durring install.
+resource "matchbox_profile" "ubuntu-18.04-netboot-install" {
   name = "ubuntu-18.04-install"
   kernel = "http://archive.ubuntu.com/ubuntu/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/linux"
   initrd = [
     "http://archive.ubuntu.com/ubuntu/ubuntu/dists/bionic/main/installer-amd64/current/images/netboot/ubuntu-installer/amd64/initrd.gz"
   ]
+  # Note the configuration is intentionally split between args = [], and generic/preseed.cfg for demonstration purposes.
+  # You could choose to use one or the other or a mixture of both.
+  # preseed.cfg is based on the preseed.cfg examples in chef/bento github repo
   args = [
     "initrd=initrd.gz",
     "locale=en_US.UTF-8",
     "keyboard-configuration/layoutcode=us",
     "hostname=foobar",
-    "preseed/url=http://matchbox.example.com:8080/assets/preseed.cfg"
+    "preseed/url=http://matchbox.example.com:8080/assets/ubuntu/bionic/preseed.cfg"
   ]
-  generic_config = "${file("./generic/preseed.cfg")}"
+  generic_config = "${file("./generic/ubuntu/bionic/preseed.cfg")}"
+}
+
+# This uses cached assets generated with `scripts/get-ubuntu bionic`
+resource "matchbox_profile" "ubuntu-18.04-asset-install" {
+  name = "ubuntu-18.04-asset-install"
+  kernel = "${var.matchbox_http_endpoint}/asset/ubuntu/bionic/linux"
+  initrd = [
+    "${var.matchbox_http_endpoint}/asset/ubuntu/bionic/initrd.gz"
+  ]
+  args = [
+    "initrd=initrd.gz",
+    "preseed/url=http://matchbox.example.com:8080/assets/ubuntu/bionic/preseed.cfg"
+  ]
+  generic_config = "${file("./generic/ubuntu/bionic/preseed.cfg")}"
 }
