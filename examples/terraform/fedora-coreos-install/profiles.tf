0fa86bc30cc7718f7bc914ddc02bb582c9c7f40a
Benjamin Gilbert <bgilbert@redhat.com>
2022-07-15 11:07:11 -0400

    Drop legacy Fedora CoreOS kernel arguments
    rd.neednet=1 is no longer needed by default.  Also, stop disabling
    cgroups v2.

--- examples/terraform/fedora-coreos-install/profiles.tf
+++ examples/terraform/fedora-coreos-install/profiles.tf
@@ -8,7 +8,6 @@ resource "matchbox_profile" "fedora-coreos-install" {
 
   args = [
     "coreos.live.rootfs_url=https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-live-rootfs.x86_64.img",
-    "rd.neednet=1",
     "coreos.inst.install_dev=/dev/sda",
     "coreos.inst.ignition_url=${var.matchbox_http_endpoint}/ignition?uuid=$${uuid}&mac=$${mac:hexhyp}",
     "console=tty0",
