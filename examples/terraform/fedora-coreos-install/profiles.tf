3193724e8a733e4edcb39cb105ec1d821da3b2eb
Benjamin Gilbert <bgilbert@redhat.com>
2022-07-15 11:06:00 -0400

    Switch Fedora CoreOS to coreos.live.rootfs_url kernel argument
    We still support passing the rootfs image as an appended initrd, but we
    recommend using coreos.live.rootfs_url instead: it generally boots faster
    and requires less RAM.

--- examples/terraform/fedora-coreos-install/profiles.tf
+++ examples/terraform/fedora-coreos-install/profiles.tf
@@ -3,11 +3,11 @@ resource "matchbox_profile" "fedora-coreos-install" {
   name   = "worker"
   kernel = "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-live-kernel-x86_64"
   initrd = [
-    "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-live-initramfs.x86_64.img",
-    "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-live-rootfs.x86_64.img"
+    "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-live-initramfs.x86_64.img"
   ]
 
   args = [
+    "coreos.live.rootfs_url=https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-live-rootfs.x86_64.img",
     "rd.neednet=1",
     "coreos.inst.install_dev=/dev/sda",
     "coreos.inst.ignition_url=${var.matchbox_http_endpoint}/ignition?uuid=$${uuid}&mac=$${mac:hexhyp}",
